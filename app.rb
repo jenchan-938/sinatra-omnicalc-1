c94b39e78389c542efc0dc37997028eddb049511
jenchan-938 <jchan5@uchicago.edu>
2024-10-25 15:54:45 +0000

    10:54

--- app.rb
+++ app.rb
@@ -7,3 +7,14 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get ("/square/new") do
+  erb(:square) 
+end
+
+get ("/square/results") do
+  @sq_no = params.fetch("number").to_i
+  pp @sq_no
+  erb(:square_results)
+
+end
